2802e6090b298b2a710e297c002f8e99027d7bf8
pgourav6464 <gouravpardeshi6464@gmail.com>
2023-10-28 01:51:36 -0700

    assigment 2 done

---
+++ assignment_2_Add_2_numbers.c
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+#include <stdio.h>
+void main(){
+
+    int num1 , num2 ;
+    printf("Enter Two Numbers:");
+   
+    scanf("%d %d" , &num1 , &num2);
+    
+    printf("Addition of %d & %d is %d" , num1 , num2 , num1+num2);
+=======
+#include <stdio.h>
+void main(){
+
+    int num1 , num2 ;
+    printf("Enter Two Numbers:");
+   
+    scanf("%d %d" , &num1 , &num2);
+    
+    printf("Addition of %d & %d is %d" , num1 , num2 , num1+num2);
+>>>>>>> e163520 (assignment 2 done)
+}
