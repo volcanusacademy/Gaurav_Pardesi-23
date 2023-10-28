ac7e2a7695afc963f36b875003337be394f1315f
Gourav pardeshi <148945163+pgourav6464@users.noreply.github.com>
2023-10-28 02:32:15 -0700

    Update assignment_2_Add_2_numbers.c

--- assignment_2_Add_2_numbers.c
+++ assignment_2_Add_2_numbers.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 void main(){
 
@@ -8,15 +7,5 @@ void main(){
     scanf("%d %d" , &num1 , &num2);
     
     printf("Addition of %d & %d is %d" , num1 , num2 , num1+num2);
-=======
-#include <stdio.h>
-void main(){
-
-    int num1 , num2 ;
-    printf("Enter Two Numbers:");
-   
-    scanf("%d %d" , &num1 , &num2);
-    
-    printf("Addition of %d & %d is %d" , num1 , num2 , num1+num2);
->>>>>>> e163520 (assignment 2 done)
 }
+
